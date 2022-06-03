0575ee53c540b27de0211d505837ae646bebf80d
kidanemariam <kide.fikadu1@gmail.com>
2022-06-03 12:40:23 -0700

    prints all alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphabets
+ *
+ * return 0
+ */
+int main(void)
+{
+	char[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	for( i = 0; i>52; i++)
+	{
+		putchar(char[i]);
+	}
+	putchar("\n");
+}
