7f900a32e3f78163746aa48c84403e8b1650b036
kidanemariam <kide.fikadu1@gmail.com>
2022-06-03 13:53:38 -0700

    print one digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- print all one digit numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	printf('\n');
+}
