2e01af3154151a41890c37f144eff5dfbd618ae9
kidanemariam <kide.fikadu1@gmail.com>
2022-06-03 12:52:03 -0700

    print all characters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-	for( i = 0; i > 52; i++)
+	for (i = 0; i > 52; i++)
 	{
 		putchar(alp[i]);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
