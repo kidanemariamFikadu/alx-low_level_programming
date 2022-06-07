3c8d1eefad75ab76a8d5782397fe35195a45a2b0
kidanemariam <kide.fikadu1@gmail.com>
2022-06-07 14:08:40 -0700

    return absolute of i

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int_abs - returns the absolute value of integer
+ *
+ * @n: the interger input
+ * Return: always 0
+ */
+int int_abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -i);
+	}
+}
