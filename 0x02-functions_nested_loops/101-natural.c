f04c48dd36695253d8845b3d98d67ed417a72075
kidanemariam <kide.fikadu1@gmail.com>
2022-06-07 16:13:15 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print natural numbers below 1024 multiple of 3 and 5
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if((a % 3) == || (a % 5) ++ 0)
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
