36002df8b950f3df6cc2fa7c35e65cd248250c43
kidanemariam <kide.fikadu1@gmail.com>
2022-06-07 13:14:10 -0700

    is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * _islower - prints uppercase or lowercase 
+ * Return: always 0
+ */
+int _islower(int c)
+{
+	if(c < 96 && c > 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
