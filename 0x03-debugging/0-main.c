3e8382dc0edcd4fbfe539fea6793429a29d951cd
kidanemariam <kide.fikadu1@gmail.com>
2022-06-09 15:31:33 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check code
+ * Return: always o
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	retutn (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h_
+#define main_h_
+
+void positive_or_negative(int i);
+
+#endif
