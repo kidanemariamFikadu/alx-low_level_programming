2f8acaafa4b1af611e603856a2205a425ceb6b33
kidanemariam <kide.fikadu1@gmail.com>
2022-06-09 13:08:41 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 48; j <= 52; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if(j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
