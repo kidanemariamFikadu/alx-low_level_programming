e645e052270d77d122ac2cfecc2666039378e54e
kidanemariam <kide.fikadu1@gmail.com>
2022-06-09 17:01:42 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,13 +10,16 @@ int main(void)
 
 	n1 = 1;
 	n2 = 2;
+	tot = atot = 0;
 	while (tot <= 40000000)
 	{
 		tot = n1 + n2;
 		n1 = n2;
 		n2 = tot;
 		if ((n1 % 2) == 0)
+		{
 			atot += n1;
+		}
 	}
 	printf("%ld\n", atot);
 	return (0);
