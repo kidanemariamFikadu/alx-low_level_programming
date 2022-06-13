342ea870654189059d2489c792630890ba9a2635
kidanemariam <kide.fikadu1@gmail.com>
2022-06-13 14:52:17 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print reverse text
+ * @s: string input
+ * Return: void
+ */
+
+void print_rev(str *s)
+{
+	int c = 0;
+
+	while (c >= 0)
+	{
+		if (s[c] == '\0')
+			break;
+		c++;
+	}
+
+	for (c--; c >= 0; c--)
+		_putchar(s[count]);
+	_putchar('\n');
+}
