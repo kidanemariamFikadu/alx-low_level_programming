e65fafdebcbf8755692fbd90fed25ef417b0ac37
kidanemariam <kide.fikadu1@gmail.com>
2022-06-13 15:44:45 -0700

    print n array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - print array of given amount
@@ -12,6 +13,10 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		_putchar(a[i]);
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(",");
+	}
 	_putchar('\n');
 }
