0dba80d8067feb5770dd5f3ad2cb25d102d91598
kidanemariam <kide.fikadu1@gmail.com>
2022-06-16 14:26:44 -0700

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,9 +10,9 @@ char *string_toupper(char *s)
 {
 	int count = 0;
 
-	 while (*(s + count) != '\0')
+	while (*(s + count) != '\0')
 	{
-		if((*(s + count) >= 97) && (*(s + count) <= 122))
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
 			*(s + count) = *(s + count) - 32;
 		count++;
 	}
