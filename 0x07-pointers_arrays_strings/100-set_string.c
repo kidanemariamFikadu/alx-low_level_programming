7a0ef609074a277cc9fe51b896c98ae83dd3c083
kidanemariam <kide.fikadu1@gmail.com>
2022-06-20 14:33:25 -0700

    set string'

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the vale of the ponter ro a char
+ * @s: source address
+ * @to: target address
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
