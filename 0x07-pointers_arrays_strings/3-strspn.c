fc59f91bd9750c7c9303a71ab33a536c14c8d641
kidanemariam <kide.fikadu1@gmail.com>
2022-06-20 13:26:14 -0700

    string length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,16 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, bool;
 
-	for (i = 0; *(s + i) != '\0'; j++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if (*(s + i) == *(accept + j))
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			bool = 0;
-			break;
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
 		}
 		if (bool == 1)
 			break;
