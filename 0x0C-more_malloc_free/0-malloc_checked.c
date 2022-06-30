e1b9d8a387bc375bebc0e16696defda1e8ad7e8c
kidanemariam <kide.fikadu1@gmail.com>
2022-06-30 13:24:13 -0700

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: amount of bytes
+ * Return pointer or 98 when no space
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef main_h__
+#define main_h__
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif
