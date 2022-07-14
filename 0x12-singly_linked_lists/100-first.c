8db4310f2a7d4f01c3f248466916d7f6abceab5f
kidanemariam <kide.fikadu1@gmail.com>
2022-07-14 21:33:15 +0000

    first print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function excuted before main
+ * Return: no return
+ */
+void __attribute__ ((constructor)) bmain()
+{
+	printf("you're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
