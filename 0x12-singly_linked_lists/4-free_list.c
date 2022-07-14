f0470ad07ecd0657560933feefda58d531df6052
kidanemariam <kide.fikadu1@gmail.com>
2022-07-14 21:28:48 +0000

    free linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - will free a signle linked list
+ * @head: head of the linked list
+ * Return: no return
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
