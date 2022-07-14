bb693a1d2a8a0cff6f6c4848ff005ea966a7684a
kidanemariam <kide.fikadu1@gmail.com>
2022-07-14 21:41:30 +0000

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,5 @@
  */
 void __attribute__ ((constructor)) bmain()
 {
-	printf("you're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
