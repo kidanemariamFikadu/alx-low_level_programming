c6c4b420c2a6d63f2ac0b3550b517289732a5d8f
kidanemariam <kide.fikadu1@gmail.com>
2022-07-14 21:10:52 +0000

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
  * @str: node to be added
  * Return: linked list with the new node
  */
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	size_t nchar;
@@ -13,6 +13,12 @@ list_t *add_node(list_t **head, const char *str);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
+	new->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
 	new->len = nchar;
 	new->next = *head;
 	*head = new;
