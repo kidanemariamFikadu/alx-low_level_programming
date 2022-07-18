b7d8cb7c301c9ec210cd0481535cc1fd98dbd260
kidanemariam <kide.fikadu1@gmail.com>
2022-07-18 17:46:36 +0000

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,8 +15,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 	}
 	temp->n = n;
-	temp->next = head;
-	head = temp;
+	temp->next = *head;
+	*head = temp;
 	free(temp);
 	return (head);
 }
