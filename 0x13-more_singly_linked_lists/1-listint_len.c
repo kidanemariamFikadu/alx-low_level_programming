b61755008f5aee11bdc4450b0b61c683a671857e
kidanemariam <kide.fikadu1@gmail.com>
2022-07-18 17:33:04 +0000

    number of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints length of list
+ * @h: linked list input
+ * Return: number of nodes
+ */
+size_t listint_len(const listinit_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
+}
