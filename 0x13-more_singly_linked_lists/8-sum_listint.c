bc432843327e33fcb9930ca442f97b53feded1fa
kidanemariam <kide.fikadu1@gmail.com>
2022-07-19 19:58:24 +0000

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data
+ * @head: head of the list
+ * Return: sum of all the data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
